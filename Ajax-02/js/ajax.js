6bc7c5da2fc7e8d154038aa9031007522bb51c66
Daniela <daniela.londono@arbusta.net>
2022-07-05 17:59:00 -0300

    recorriendo el array del objeto que traigo de mi json, y poniendolo en una tabla

---
+++ Ajax-02/js/ajax.js
@@ -0,0 +1,53 @@
+(function () {
+
+
+	$.ajax({
+		type: 'GET',
+		url: 'json/datos.json',
+		dataType: 'json'
+	})
+		.done(function (data) {
+
+			var personas = data;
+
+			//recorro mi array de personas
+			for (let i = 0; i < personas.length; i++) {
+
+				var persona = personas[i];
+				var tags = "";
+
+				//recorro los tags de personas, un array dentro de otro array
+				for (let j = 0; j < persona.tags.length; j++) {
+					tags += '<span class="label label-primary"> ' + persona.tags[j] + '</span> ';
+					console.log(tags)
+				}
+
+				let html = "";
+				html += '<tr>'
+				html += '<td> ' + persona.name + '</td>';
+				html += '<td>' + persona.age + '</td>';
+				html += '<td> ' + tags + ' </td>';
+				html += '</tr>';
+
+				$('.table').append(html);
+			}
+
+
+
+
+		})
+		.fail(function () {
+			console.log("Fallo!");
+		})
+		.always(function () {
+			console.log("Completo!");
+		});
+
+
+
+
+
+
+
+
+})();
